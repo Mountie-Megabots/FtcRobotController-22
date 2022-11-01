e9bf4dfd1a6ef2b499c25efafc3c55d5ae030e78
Hayden <114115430+Hayden0718@users.noreply.github.com>
2022-11-01 19:24:19 -0400

    Create Redauto.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Redauto.java
@@ -0,0 +1,148 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.RenderNode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@Autonomous
+public class Redauto extends LinearOpMode {
+    private Blinker control_Hub;
+    private DcMotor b_left;
+    private DcMotor b_right;
+    private DcMotor f_left;
+    private DcMotor f_right;
+    private DcMotor armMotor;
+    private Servo servo;
+    private BNO055IMU imu;
+    double servoSetting = 0;
+    double headingOffset = 0;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        control_Hub = hardwareMap.get(Blinker.class, "Control Hub");
+        b_left = hardwareMap.get(DcMotor.class, "b_left");
+        b_right = hardwareMap.get(DcMotor.class, "b_right");
+        f_left = hardwareMap.get(DcMotor.class, "f_left");
+        f_right = hardwareMap.get(DcMotor.class, "f_right");
+        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+        servo = hardwareMap.get(Servo.class, "servo");
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+        imu.initialize(parameters);
+
+        b_left.setDirection(DcMotor.Direction.FORWARD);
+        b_right.setDirection(DcMotor.Direction.REVERSE);
+        f_left.setDirection(DcMotor.Direction.FORWARD);
+        f_right.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive()) {
+            /*
+            // Step 1:  Drive forward for 3 seconds
+            drive(1, 0, 0, false);
+            runtime.reset();
+            while (opModeIsActive() && (runtime.seconds() < 3.0)) {
+                telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+                telemetry.update();
+            }
+
+            // Step 2:  Spin right for 1.3 seconds
+            drive(0, 0, -1, false);
+            runtime.reset();
+            while (opModeIsActive() && (runtime.seconds() < 1.3)) {
+                telemetry.addData("Path", "Leg 2: %4.1f S Elapsed", runtime.seconds());
+                telemetry.update();
+            }
+
+            // Step 3:  Drive Backward for 1 Second
+            drive(-1, 0, 0, false);
+            runtime.reset();
+            while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+                telemetry.addData("Path", "Leg 3: %4.1f S Elapsed", runtime.seconds());
+                telemetry.update();
+            }
+            */
+
+            // Step 1:  Drive left for 3 seconds
+            drive(0, -1, 0, false);
+            runtime.reset();
+            while (opModeIsActive() && (runtime.seconds() < 3.0)) {
+                telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+                telemetry.update();
+            }
+
+            // Step 4:  Stop
+            drive(0, 0, 0, false);
+
+            telemetry.addData("Path", "Complete");
+            telemetry.update();
+
+            telemetry.addData("Status", "Running");
+            telemetry.addData("Servo-Status", servoSetting);
+            telemetry.addData("Gyro", getHeading());
+            telemetry.update();
+        }
+    }
+
+    public void drive(double y, double x, double rx, boolean fieldRelative) {
+        double rotX;
+        double rotY;
+
+        // Read inverse IMU heading, as the IMU heading is CW positive
+        double botHeading = getHeading();
+
+        if (fieldRelative) {
+            rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+            rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
+        } else {
+            rotX = x;
+            rotY = y;
+        }
+
+
+        telemetry.addData("RotX", rotX);
+        telemetry.addData("RotY", rotY);
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+
+        f_left.setPower(frontLeftPower);
+        b_left.setPower(backLeftPower);
+        f_right.setPower(frontRightPower);
+        b_right.setPower(backRightPower);
+    }
+
+    public double getHeading() {
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC,
+                AxesOrder.ZYX, AngleUnit.DEGREES);
+        return -angles.firstAngle - headingOffset;
+    }
+}
